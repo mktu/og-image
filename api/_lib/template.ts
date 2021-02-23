497751c9fb2beec788176c8305dcc80be54ea37c
mktu <c1ow11er75@gmail.com>
2021-02-23 21:32:09 +0900

    add googlefont to heading

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -96,7 +96,7 @@ function getCss(theme: string, fontSize: string) {
     }
     
     .heading {
-        font-family: 'Inter', sans-serif;
+        font-family: 'M PLUS 1p', 'Inter', sans-serif;
         font-size: ${sanitizeHtml(fontSize)};
         font-style: normal;
         color: ${foreground};
